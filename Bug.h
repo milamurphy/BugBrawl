e8492f7ca601ca537074a613bf8645c167bfe5e0
milamocha <d00253640@student.dkit.ie>
2024-04-16 16:47:12 +0100

    added bug, crawler and hopper classes and methods in crawler.cpp

---
+++ Bug.h
@@ -0,0 +1,25 @@
+#ifndef CA2_BUG_H
+#define CA2_BUG_H
+#include <utility>
+#include <list>
+using namespace std;
+
+class Bug
+{
+protected:
+    int id{};
+    pair<int, int> position;
+    int direction{};
+    int size{};
+    bool alive{};
+    list<pair<int,int>> path;
+
+public:
+    // Bug(int id=0);	// parameter, with default value
+    virtual void move() =0;
+    virtual bool isWayBlocked() =0;
+
+    ~Bug();  // destructor
+};
+
+#endif
