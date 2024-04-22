0b46f9a2b98ade9ac7f5b71cc1e853593f759889
milamocha <d00253640@student.dkit.ie>
2024-04-22 10:02:05 +0100

    remove repeated code in main

--- main.cpp
+++ main.cpp
@@ -49,102 +49,11 @@ int main() {
     } while (choice != 8);
     fin.close();
 
-    vector<Bug*> bugs_vector; // pointer to Bug
-    readFile(bugs_vector);
-    // Create vector of 100 cells in Board.cpp, each cell contains a vector of bugs
 
-    printBugs(bugs_vector);
+    // Create vector of 100 cells in Board.cpp, each cell contains a vector of bugs
 
 
     return 0;
-}
-
-void readFile(vector<Bug*>& bugs_vector)
-{
-    ifstream fin("bugs.txt");
-    if(fin.good()) {
-        string line;
-
-        while (getline(fin, line))
-        {
-            parseLine(line, bugs_vector);
-        }
-        fin.close();
-    }
-    else {
-        cout << "Unable to open file, or file is empty." << endl;
-    }
-}
-
-void parseLine(const string& strLine, vector<Bug*>& bugs_vector)
-{
-    stringstream strStream( strLine ); // turn the line of text into a stringstream for tokenizing
-    const  char DELIMITER = ';';
-    string bug_type;
-    getline(strStream, bug_type, DELIMITER);
-    string strTemp; // temporary string
-
-    try {
-        if (bug_type == ("C")) {
-            // bugtype, id, x, y, direction, size)
-            getline(strStream, strTemp, DELIMITER);
-            int id = stoi(strTemp); // stoi converts string to int
-            getline(strStream, strTemp, DELIMITER);
-            int x = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int y = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int direction = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int size = stoi(strTemp);
-
-            pair<int, int> position = make_pair(x, y);
-            //bool isAlive = true;
-            //list<pair<int, int>> path = {};
-
-            bugs_vector.push_back(new Crawler(id, position, direction, size));
-        } else if (bug_type == ("H")) {
-            // bugtype, id, x, y, direction, size, hopLength)
-            getline(strStream, strTemp, DELIMITER);
-            int id = stoi(strTemp); // stoi converts string to int
-            getline(strStream, strTemp, DELIMITER);
-            int x = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int y = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int direction = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int size = stoi(strTemp);
-            getline(strStream, strTemp, DELIMITER);
-            int hopLength = stoi(strTemp);
-
-            pair<int, int> position = make_pair(x, y);
-            //bool isAlive = true;
-            //list<pair<int, int>> path = {};
-
-            bugs_vector.push_back(new Hopper(id, position, direction, size, hopLength));
-        }
-    }
-    catch (std::invalid_argument const& e)
-    {
-        cout << "Bad input: std::invalid_argument thrown" << '\n';
-    }
-    catch (std::out_of_range const& e)
-    {
-        cout << "Integer overflow: std::out_of_range thrown" << '\n';
-    }
-}
-
-void printBugs(const vector<Bug*>& bugs_vector) {
-    cout << "Printing Bugs:" << endl;
-    for (const auto& bug : bugs_vector) {
-        cout << "Bug Type: " << (typeid(*bug) == typeid(Crawler) ? "Crawler" : "Hopper") << endl;
-        cout << "ID: " << bug->getId() << endl;
-        cout << "Position: (" << bug->getPosition().first << ", " << bug->getPosition().second << ")" << endl;
-        cout << "Direction: " << bug->getDirection() << endl;
-        cout << "Size: " << bug->getSize() << endl;
-        cout << endl;
-    }
 }
 
 void displayMenu() {
